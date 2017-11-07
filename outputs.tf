2e223177ec05b0606cf483ecf157287c069d0474
charles-d-burton <charles.burton@mindflash.com>
2017-11-07 16:27:16 -0700

    added target group output

--- outputs.tf
+++ outputs.tf
@@ -5,3 +5,7 @@ output "https_arn" {
 output "http_arn" {
   value = "${aws_lb_listener.front_end_http.arn}"
 }
+
+output "target_group_arn" {
+  value = "${aws_lb_target_group.alb_target_group.arn}"
+}
